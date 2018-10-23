9d1fa8e59de66e700602cd20ce1f49f1843921b4
陆泽辉luzh <zehui.lzh@alibaba-inc.com>
2018-10-23 14:18:15 +0800

    add TemplateMethodMode

---
+++ TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/AbstractDisplay.cpp" "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/AbstractDisplay.cpp.o"
+  "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/CharDisplay.cpp" "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/CharDisplay.cpp.o"
+  "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/StringDisplay.cpp" "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/StringDisplay.cpp.o"
+  "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/main.cpp" "/Users/luzh/nodeAddon/DesignPatterns/TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TemplateMethodMode/cmake-build-debug/CMakeFiles/TemplateMethodMode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplateMethodMode.dir/AbstractDisplay.cpp.o"
+  "CMakeFiles/TemplateMethodMode.dir/main.cpp.o"
+  "CMakeFiles/TemplateMethodMode.dir/CharDisplay.cpp.o"
+  "CMakeFiles/TemplateMethodMode.dir/StringDisplay.cpp.o"
+  "TemplateMethodMode.pdb"
+  "TemplateMethodMode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplateMethodMode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
